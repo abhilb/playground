c24886770c3e7e52b9fb18bf3521e5f7b250e3d6
abhilash <abhilashbabuj@gmail.com>
2017-03-19 10:19:04 +0000

    Added a ruby playground

---
+++ ruby_playground/coursera/module1/basics.rb
@@ -0,0 +1,18 @@
+
+#Getting started with Ruby!!!
+
+# Print hello world three times!
+3.times { puts "Hello World!"}
+
+# case statements
+
+a = 10
+puts a
+case a
+when a > 0 then puts "Positive"
+when a < 0 then puts "Negative"
+when a == 0 then puts "Zero"
+end
+
+
+
