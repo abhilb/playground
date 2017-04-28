c6fbaf1c589a3072f2c34ddd9669a2da6a52fee1
abhilash <abhilashbabuj@gmail.com>
2017-04-28 20:47:57 +0000

    Added more playgrounds

---
+++ lisp_playground/ch01.lisp
@@ -0,0 +1,5 @@
+
+(write-line "Hello world")
+
+(write (+ 3 (* 4 2)))
+
---
+++ python_playground/04_excel/data_retrieve.py
@@ -0,0 +1,20 @@
+import xlrd
+
+def main():
+    """ Program to understand the capabilities of the xlrd module of python.
+    Basically this module is used to read data from excel sheet. It supports 
+    both old format (.xls) and the new format (.xlsx) of excel sheet. 
+    For this purpose I am using the dataset from German Deutsch bahn. 
+    It is provided at the following link. 
+
+
+    History:
+    25th April, 2017: abhilash Initial version
+    """
+    
+    print("Program to parse excel sheets")
+
+
+
+if __name__ == '__main__':
+    main()
