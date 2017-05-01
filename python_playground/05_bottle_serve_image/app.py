1f3e56a6d1f76b42c5252b326551e26bbb0833b6
abhilash <abhilashbabuj@gmail.com>
2017-05-01 13:32:43 +0000

    Now we have a working version that loads an image from db

--- python_playground/05_bottle_serve_image/app.py
+++ python_playground/05_bottle_serve_image/app.py
@@ -1,5 +1,6 @@
 from bottle import run, request, route, template, Bottle, static_file
 import os
+import sqlite3
 
 app = Bottle()
 root_path = os.path.dirname(os.path.realpath(__file__))
@@ -13,9 +14,41 @@ def server_static(filename):
     print "sending " + filename
     return static_file(filename, root=static_files_path)
 
+@app.route('/getimage')
+def getimage():
+    print "Got get image request received... "
+    conn = sqlite3.connect('test.db')
+    cur = conn.cursor()
+
+    cur.execute('''select image from images''')
+    blob = cur.fetchone()
+
+    print len(blob[0])
+    print type(blob)
+
+    cur.close()
+    conn.close()
+
+    return blob[0]
+
 @app.route('/')
 def index():
     return template('index')
 
 if __name__ == '__main__':
+    if(os.path.exists('test.db')):
+            os.remove('test.db')
+    conn = sqlite3.connect('test.db')
+    conn.execute('''
+    CREATE TABLE images 
+    (key TEXT PRIMARY KEY, image BLOB);
+    ''')
+
+    with open(os.path.join(static_files_path, "testimage.png"), 'rb') as f:
+        blob = f.read()
+        conn.execute('''INSERT INTO images (image) VALUES(?);''', [sqlite3.Binary(blob)])
+
+    conn.commit()
+    conn.close()
+
     run(app, Debug=True, reloader=True)    
--- python_playground/05_bottle_serve_image/static/app.js
+++ python_playground/05_bottle_serve_image/static/app.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
     $('#getBtn').click(function(){
         console.log("Button clicked");
         $(this).parent().append("<p>Image loaded from db</p>");
+
+        var imgTag = $('<img>');
+        imgTag.attr("height", "50");
+        imgTag.attr("width", "50");
+        imgTag.attr("src", "/getimage");
+        $(this).parent().append(imgTag);
     });
 
 });
