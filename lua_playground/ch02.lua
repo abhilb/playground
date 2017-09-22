235a535c9fcf716627880a41352adca2b8450821
abhilb <abhilashbabuj@gmail.com>
2017-09-23 00:19:00 +0200

    Chapter 3

---
+++ lua_playground/ch02.lua
@@ -0,0 +1,25 @@
+
+
+
+-- Chapter 02 Types and values
+
+
+-- There are basically 8 types in lua
+-- ni
+-- boolean
+-- number
+-- string
+-- userdata
+-- thread
+-- function
+-- table
+--
+-- One can find the type of the value
+-- using the type keyword.
+
+print(type("Abhilash")) -- string
+print(type(10.2))       -- number
+print(type(print))      -- function
+print(type(true))       -- boolean
+print(type(nil))        -- nil
+
---
+++ lua_playground/ch03.lua
@@ -0,0 +1,10 @@
+#!/usr/bin/lua
+
+-- Loops in lua
+-- 1. for loop
+
+
+for i=1,10 do
+    print(i)
+end
+
